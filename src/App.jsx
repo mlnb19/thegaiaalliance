adaa63ea1c2d38b86d31c1c8cbefea677cc05b1b
yvettefilipsso1 <30127700-yvettefilipsso1@users.noreply.replit.com>
2024-12-20 22:49:16 +0000

    Automatic commit before Assistant edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import Startpage from './pages/Startpage';
+import Sealevels from './pages/Sealevels';
 import ProfilePage from './pages/ProfilePage';
 import NotificationsPage from './pages/NotificationsPage';
 import SettingsPage from './pages/SettingsPage';
@@ -12,7 +12,7 @@ function App() {
     <ChakraProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Startpage />} />
+          <Route path="/" element={<Sealevels />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/notifications" element={<NotificationsPage />} />
           <Route path="/settings" element={<SettingsPage />} />
