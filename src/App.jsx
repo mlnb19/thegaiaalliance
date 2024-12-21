788d06eeb4673d200b8398abeacc682eb2b60378
yvettefilipsso1 <30127700-yvettefilipsso1@users.noreply.replit.com>
2024-12-21 20:00:52 +0000

    Automatic commit before Assistant edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import SeaLevels from './pages/SeaLevels';
